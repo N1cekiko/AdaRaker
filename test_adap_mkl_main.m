bf3f4f4a64577d8964e160491d9d8dc746ec951a
yanningshen <50156124+yanningshen@users.noreply.github.com>
2019-05-20 00:07:31 -0500

    Add files via upload

---
+++ test_adap_mkl_main.m
@@ -0,0 +1,57 @@
+clear all;
+close all;
+
+
+load AirData
+
+
+
+
+T=length(y);
+N=size(X,1);
+ 
+c1=[-1:3];
+ c1=0;
+ cs1=10.^c1;
+%cs1=1;
+
+for i=1:length(c1);
+
+    all_step=cs1(i)/sqrt(T);
+    all_beta=0.5;
+    % all_lambda=st*T;
+    all_lambda=0.01;
+    all_cplx=50;
+    
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Strongly adaptive OMKL with random feature approximation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        
+params5=struct;
+params5.N=N; params5.T=T;  params5.ker_list={'rbf'}; n_ker=size(params5.ker_list,2);
+params5.beta=all_beta; params5.delta=0.1;   params5.eta=all_step; params5.w_ini=0*ones(1,n_ker); 
+params5.lambda=all_lambda;   params5.sigma=[ 0.1 1 10]; params5.L=all_cplx;  params5.theta_ini=ones(length(params5.sigma),1)/length(params5.sigma);
+params5.S=2;
+
+
+    tic;
+    [f_rf,theta_rf,w_hat,er_rf,erm_rf]=mkl_adap_online_rf(y,X,params5);        
+    t_adaprf=toc;
+    E_adaraker(i)=erm_rf(end);
+    
+    
+
+
+
+
+
+end
+
+
+
+    fprintf('AdaRaker=%f\n\n', min(E_adaraker));
+
+    
